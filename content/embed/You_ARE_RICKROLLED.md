3a0b10eec74115bdbe79530db3dbe9a684373484
ja-errorpro <jacky8980@gmail.com>
2022-10-20 00:40:11 +0800

    add rickroll

---
+++ content/embed/You_ARE_RICKROLLED.md
@@ -0,0 +1,12 @@
+---
+title: "You are Rickrolled"
+url: "/embed/rickroll"
+
+
+---
+
+{{< adiv >}}
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0"></iframe>
+
+{{< /adiv >}}
--- static/js/a_secret_script.js
+++ static/js/a_secret_script.js
@@ -1,15 +1,22 @@
-var encryptedflag = "9bd69bc521da9d77206b9cc613fea2ba";
-
+var encryptedflag1 = "9bd69bc521da9d77206b9cc613fea2ba";
+var encryptedflag2 = "8f6e10638e795ed1e58b19304fd0036a";
 function on_bt_clicked() {
 	var input = document.getElementsByName("input")[0];
 	let text = input.value;
 	text = Base64.encode(text);
 	text = md5(text);
-	
-	if(text==encryptedflag){
-		
+	console.log(text)
+	if(text==encryptedflag1){
 		alert("Cool! You find a flag!");
-	}else{
+	}
+	else if(text == encryptedflag2){
+		alert("Yeah, but this is cooler!")
+		var url =  "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+		
+		
+		window.open(url)
+	}
+	else{
 		alert("no such file, directory, or command ...");
 	}
 	
